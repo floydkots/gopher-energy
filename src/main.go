dd1ff8b002fd2a612bcb477f7e604ef0f23b402a
Floyd Kots <floydkotohoyoh@gmail.com>
2018-06-02 22:03:05 +0300

    Make it menu driven

--- src/main.go
+++ src/main.go
@@ -3,19 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	var plantCapacities []float64
 
-	plantCapacities = []float64{30, 30, 30, 60, 60, 100}
+	plantCapacities := []float64{30, 30, 30, 60, 60, 100}
+	activePlants := []int{0, 1}
+	gridLoad := 75.
 
-	var capacity float64 = plantCapacities[0] + plantCapacities[1] +
-		plantCapacities[2] + plantCapacities[3] + plantCapacities[4] +
-		plantCapacities[5]
+	fmt.Println("1) Generate Power Plant Report")
+	fmt.Println("2) Generate Power Grid Report")
+	fmt.Print("Please choose an option: ")
 
-	var gridLoad = 75.
+	var option string
 
-	utilization := gridLoad / capacity
+	fmt.Scanln(&option)
 
-	fmt.Printf("%-20s%.0f\n","Capacity: ", capacity)
-	fmt.Printf("%-20s%.0f\n","Load: ", gridLoad)
-	fmt.Printf("%-20s%.1f%%","Utilization", utilization * 100)
+	switch option {
+	case "1":
+		for idx, cap := range plantCapacities {
+			fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
+		}
+
+	case "2":
+		capacity := 0.
+		for _, plantId := range activePlants {
+			capacity += plantCapacities[plantId]
+		}
+
+		fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+		fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+		fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+
+	default:
+		fmt.Println("Unknown option, no action taken.")
+	}
 }
